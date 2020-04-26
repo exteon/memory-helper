baf229bd2e7c5992ef8c961338312e86f68bdc41
Dinu Marina <dinumarina@gmail.com>
2020-04-26 16:33:33 +0300

    Cleanups

--- build/build.sh
+++ build/build.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p $DIR/temp
 
 # Build docs
-mkdir -p $DIR/temp/phpdoc-cache
-$DIR/../vendor/bin/phpdoc -t $DIR/../docs --cache-folder $DIR/temp/phpdoc-cache -d $DIR/../interfaces -d $DIR/../src --template="xml"
-$DIR/../vendor/bin/phpdoc -t $DIR/../docs --cache-folder $DIR/temp/phpdoc-cache -d $DIR/../interfaces -d $DIR/../src --template="clean"
-mkdir -p $DIR/../docs/md
-$DIR/../vendor/bin/phpdocmd $DIR/../docs/structure.xml $DIR/../docs/md
+mkdir -p "$DIR/temp/phpdoc-cache"
+"$DIR/../vendor/bin/phpdoc" -t "$DIR/../docs" --cache-folder "$DIR/temp/phpdoc-cache" -d "$DIR/../interfaces" -d "$DIR/../src" --template=xml
+"$DIR/../vendor/bin/phpdoc" -t "$DIR/../docs" --cache-folder "$DIR/temp/phpdoc-cache" -d "$DIR/../interfaces" -d "$DIR/../src" --template=clean
+mkdir -p "$DIR/../docs/md"
+"$DIR/../vendor/bin/phpdocmd" "$DIR/../docs/structure.xml" "$DIR/../docs/md"
